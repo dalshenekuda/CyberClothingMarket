10fb53083898db8e23bd28419ed882d6376806c5
dalshenekuda <51684531+dalshenekuda@users.noreply.github.com>
2021-06-22 14:05:12 +0300

    shoping cart

---
+++ react_mern_shop/src/store.js
@@ -0,0 +1,22 @@
+import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
+
+import thunk from "redux-thunk";
+import {productDetailsReducer, productListReducer} from "./reducers/productReducers";
+import {cartReducer} from "./reducers/cartReducers";
+
+const initialState = {}
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart:cartReducer
+})
+
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    initialState,
+    composeEnhancer(applyMiddleware(thunk)))
+
+export default store;
