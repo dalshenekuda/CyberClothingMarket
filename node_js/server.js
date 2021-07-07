492aac5bc6a60bb85278211463c0a5a04ca34f2b
dalshenekuda <51684531+dalshenekuda@users.noreply.github.com>
2021-07-08 02:23:17 +0300

    order/pay/userPage

--- node_js/server.js
+++ node_js/server.js
@@ -1,36 +1,36 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import userRouter from "./routers/userRouter.js";
-import productRouter from "./routers/productRouter.js";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
-dotenv.config()
+dotenv.config();
+
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/mernshop', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-})
-
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
-
-app.get('/',(req, res)=>{
-    res.json({message:'Server is running'})
-})
-
-
-app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+app.get('/', (req, res) => {
+  res.send('Server is ready');
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
-const port = process.env.PORT || 5000
-app.listen(port,()=> {
-    console.log('Serve at :5000 ')
-})
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
